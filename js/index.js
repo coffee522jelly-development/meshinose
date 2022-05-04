aa2685b550c23dd7698f9841496eff791873efc0
coffee <coffeejellydev@gmail.com>
2022-05-04 19:45:26 +0900

    調整
    CSS調整

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $(function(){
   var pagetop = $('#pagetop');
   pagetop.hide();
   $(window).scroll(function () {
-     if ($(this).scrollTop() > 100) {
+     if ($(this).scrollTop() > 900) {
           pagetop.fadeIn();
      } else {
           pagetop.fadeOut();
